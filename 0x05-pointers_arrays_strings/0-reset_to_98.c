46bf42981365ee06e606fe17374c171223e97846
amanual <amanualbkl@gmail.com>
2022-12-19 12:58:46 -0800

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,9 @@
 
 /**
  * rest_to_98 -> a function to update the reference of a pointer
- * @n: pointer to n to be updated
- *
+ * @n: pointer to n to be updated*
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 		*n = 98;
 }
